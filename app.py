9ef3033bdbbabbf4f9e5eeaf5580891c9a9ca482
fredericoguimaraes <82128682+fredericoguimaraes@users.noreply.github.com>
2021-09-16 17:50:16 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+import matplotlib.pyplot as plt
+# Define o título do Dashboard
+st.title("Mini-Projeto 2 - Data App")
+st.title("Dashboard Financeiro Interativo e em Tempo Real Para Previsão de Ativos Financeiros")
