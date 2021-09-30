fe9d9c2ce64e140c1a57186aef2db773ccd6661a
fredericoguimaraes <82128682+fredericoguimaraes@users.noreply.github.com>
2021-09-30 15:04:49 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,4 +1,85 @@
+import numpy as np
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from plotly import graph_objs as go
+from datetime import date
+import warnings
 import streamlit as st
-# Define o título do Dashboard
-st.title("Oi, mo!")
-st.title("Agora sim você consegue ver, né?")
+
+dados = pd.read_excel('G:\\Meu Drive\\PYTHON\\projeto_uff\\eng_agricola.xlsx')
+
+l_colunas = [ 'Horário Seg', 'Horário Ter', 'Horário Qua', 'Horário Qui', 'Horário Sex','Horário Sáb']
+
+for i in l_colunas:
+    horario1 = []
+    horario2 = []
+    for n in range(len(dados)):
+
+        try:
+            horario1.append(int(dados[i][n].split("-")[0].split(":")[0]))
+            horario2.append(int(dados[i][n].split("-")[1].split(":")[0]))
+        except:
+            horario1.append("-")
+            horario2.append("-")
+        h1 = i +" "+ "inicial"
+        h2 = i + " " + "final"
+    dados[h1] = horario1
+    dados[h2] = horario2
+
+
+selec = ['Código Disciplina', 'Nome Disciplina', 'Código Turma',
+        'CH Teóricas', 'CH Práticas', 'CH Estágio',
+        'Horário Seg inicial', 'Horário Seg final',
+       'Horário Ter inicial', 'Horário Ter final', 'Horário Qua inicial',
+       'Horário Qua final', 'Horário Qui inicial', 'Horário Qui final',
+       'Horário Sex inicial', 'Horário Sex final', 'Horário Sáb inicial',
+       'Horário Sáb final']
+colunas = dados.columns
+b = colunas.isin(selec)
+filtro = colunas[b]
+dados_filtrado = dados[filtro]
+
+col = ['Horário Seg inicial','Horário Ter inicial','Horário Qua inicial','Horário Qui inicial','Horário Sex inicial']
+classificacao = [0 for a in  range(len(dados_filtrado))]
+for i in col:
+    for j in range(len(dados)):
+        try:
+            if int(dados_filtrado[i][j]) < 12:
+                turno = "Manhã"
+            if int(dados_filtrado[i][j]) >= 12 and int(dados_filtrado[i][j]) < 18:
+                turno = "Tarde"
+            if int(dados_filtrado[i][j]) >=18:
+                turno = "Noite"
+
+            classificacao[j] = turno
+        except:
+            pass
+
+
+dados_filtrado['classificacao'] = classificacao
+dados_filtrado['classificacao'].value_counts()
+
+
+
+
+d_graf2 = dados_filtrado[['classificacao','Código Disciplina']].groupby(dados_filtrado['classificacao']).count()/len(dados_filtrado)
+w = 0.25
+x = np.arange(len(d_graf2))
+layout = go.Layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+fig = go.Figure(data=[
+    go.Bar(name="teste",x=['tarde'],y=[d_graf2['classificacao'][0]]),go.Bar(name="teste1",x=['manha'],y=[d_graf2['classificacao'][1]]),go.Bar(name="teste2",x=['noite'],y=[d_graf2['classificacao'][2]])
+],layout=layout)
+fig.update_xaxes(visible=False)
+fig.update_yaxes(visible=False)
+fig.update_layout_images(visible=False)
+
+
+st.title("Mini-Projeto 2 - Data App")
+st.title("Dashboard Financeiro Interativo e em Tempo Real Para Previsão de Ativos Financeiros")
+
+st.plotly_chart(fig)
