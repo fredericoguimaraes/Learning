9a03d7954a7943bb438ab56ffeb472ccf5793d55
fredericoguimaraes <82128682+fredericoguimaraes@users.noreply.github.com>
2021-09-30 15:20:37 -0300

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-
-import streamlit as st
-import matplotlib.pyplot as plt
-# Define o título do Dashboard
-st.title("Mini-Projeto 2 - Data App")
-st.title("Dashboard Financeiro Interativo e em Tempo Real Para Previsão de Ativos Financeiros")
